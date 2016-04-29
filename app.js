72346f299b22a7acabcf720c7a5886fb88ec1124
Mateusz Rzepa <mrzepa89@gmail.com>
2016-04-29 21:39:21 +0200

    improved web creator mechanism

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 'use strict';
 
 var itemsapi = require('itemsapi');
-
 var winston = require('winston')
 itemsapi.get('logger').info('it works!')
+var ItemsAPI = require('itemsapi-node');
+var Promise = require('bluebird');
+var request = Promise.promisifyAll(require('request'));
+var _ = require('lodash');
+var bodyParser = require('body-parser');
+var storage = require('node-persist');
+var express = require('express');
+var nunjucks = require('nunjucks');
 
+storage.initSync({
+  dir: __dirname + '/localstorage',
+});
+
+if (!storage.getItem('step')) {
+  storage.setItem('step', 1)
+  //storage.setItem('name')
+}
 
 var ELASTICSEARCH_URL = '127.0.0.1:9200';
 // heroku elasticsearch addon
@@ -32,25 +47,14 @@ itemsapi.init({
   }
 })
 
-var ItemsAPI = require('itemsapi-node');
-var client = new ItemsAPI('http://localhost:' + PORT + '/api/v1', 'cities');
-var Promise = require('bluebird');
-var request = Promise.promisifyAll(require('request'));
-var _ = require('lodash');
-
-
 // standard app syntax
 var app = itemsapi.get('express');
-var bodyParser = require('body-parser');
-
-var express = require('express');
 app.use('/bootstrap', express.static('node_modules/bootstrap'));
 app.use('/assets', express.static('assets'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-var nunjucks = require('nunjucks');
 var nunenv = nunjucks.configure(app.get('views'), {
   autoescape: true,
   noCache: true,
@@ -61,41 +65,54 @@ app.engine('html.twig', nunenv.render);
 app.set('view engine', 'html.twig');
 app.set('view cache', false);
 
-app.get('/', function(req, res) {
-  client.getCollections()
-  .then(function(result) {
-    //console.log(result);
-    console.log(JSON.stringify(result));
 
-    var name = result.data.items[0].name;
-    console.log(name);
+/**
+ * middleware route
+ */
+app.all('*', function(req, res, next) {
+  var client = new ItemsAPI('http://localhost:' + PORT + '/api/v1', storage.getItem('name'));
+  req.client = client;
+  //req.step = storage.getItem('step')
+  nunenv.addGlobal('step', storage.getItem('step'));
+  nunenv.addGlobal('name', storage.getItem('name'));
+  console.log(storage.getItem('step'));
+  next();
+})
+
+app.get('/', function(req, res) {
+  if (storage.getItem('step') === 1) {
     res.render('start', {});
-  })
+  } else {
+    req.client.getCollections()
+    .then(function(result) {
+      console.log(JSON.stringify(result));
+      var name = result.data.items[0].name;
+      res.render('start', {});
+    })
+  }
 });
 
 app.post('/add-collection', function(req, res) {
   var json = JSON.parse(req.body.collection)
 
-  client.addCollection(json)
+  req.client.addCollection(json)
   .then(function(result) {
-    return client.createMapping(json.name)
+    return req.client.createMapping(json.name)
   })
   .then(function(result) {
+    // if adding collection was successful we go into 2 step
+    storage.setItem('step', 2)
+    storage.setItem('name', json.name)
     res.redirect('/');
   })
   .catch(function(err) {
-    console.log(err);
     res.status(500).json({});
   })
 });
 
 app.post('/add-data', function(req, res) {
-  //var json = JSON.parse(req.body.collection)
-
   var url = req.body.url;
-
   //url: 'https://raw.githubusercontent.com/itemsapi/itemsapi-example-data/master/movies.json',
-
   request.getAsync({
     url: url,
     json: true,
@@ -105,15 +122,15 @@ app.post('/add-data', function(req, res) {
     return result.body
   })
   .then(function(items) {
-    return client.deleteAllItems()
+    return req.client.deleteAllItems()
     .then(function() {
-      return client.addBulkItems(items)
+      return req.client.addBulkItems(items)
     })
   })
   .then(function() {
+    storage.setItem('step', 3)
     res.redirect('/');
   })
-
 });
 
 itemsapi.start(function serverStart(serverInstance) {
